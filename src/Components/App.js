d8e88f99404212901e8fa6f0e2ed6419f72787fb
hennnnes <hroemmer@googlemail.com>
2017-04-01 23:22:11 +0200

    basic fetch, error handling

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
 
 import './App.css';
+//import header from './Header/Header';
+// import main from './Main/Main';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.getJson('http://api.github.com/users/' + this.state.value + '/repos');
+
+    event.preventDefault();
+  }
+
+  getJson(url) {
+    fetch(url)
+    .then(response => {
+      if (response.status !== 200) {
+        this.setState({error: 'Oops, an error occured!'})
+        return;
+      }
+
+      this.setState({error: ''})
+      return response.json();
+    })
+    .then(json => {
+      this.setState({
+        value: this.state.value,
+        data: json
+      });
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header>
-            <form action="./" method="get">
+            <form action="./" onSubmit={this.handleSubmit}>
               <label htmlFor="user">Search for Github User</label>
 
               <input type="text"
                       id="user"
-                      ref="{(input) => this.textInput = input}"
-                      />
+                      value={this.state.value}
+                      onChange={this.handleChange}
+              />
 
-                    <input type="submit" value="Search" />
+              <input type="submit" value="Submit" />
             </form>
         </header>
 
+        <main>
+          {/*  only show if repositories */}
+          <h2>{this.state.value} Repositories:</h2>
 
+          <p className="error">{this.state.error}</p>
+
+        </main>
       </div>
     );
   }
